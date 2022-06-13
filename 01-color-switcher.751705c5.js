e089cbffc4cf62b72c3295edd711c9f9683a5c21
veta41 <74262484+veta41@users.noreply.github.com>
2022-06-13 18:46:38 +0000

    Deploying to gh-pages from @ veta41/goit-js-hw-09@dde30e9a7715076f74081079fd237bdc8ba72630 🚀

---
+++ 01-color-switcher.751705c5.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let n=null;t.btnStart.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.751705c5.js.map
