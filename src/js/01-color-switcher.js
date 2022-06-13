dde30e9a7715076f74081079fd237bdc8ba72630
veta41 <74262484+veta41@users.noreply.github.com>
2022-06-13 21:45:58 +0300

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop : document.querySelector('button[data-stop]'),
+}
+
+let intervalId = null;
+
+refs.btnStart.addEventListener('click', onchangeColor);
+refs.btnStop.addEventListener('click', onBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onchangeColor() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+}, 1000);
+refs.btnStart.disabled = true;
+refs.btnStop.disabled = false;
+}
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
