b4a94969c097fa5efe71c26242a8f5caf18cfad7
veta41 <74262484+veta41@users.noreply.github.com>
2022-06-14 21:13:35 +0300

    add task 2, 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,109 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+
+
+let selectedData = '';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    selectedData = selectedDates[0];
+    if (selectedData < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future', {
+        width: '350px',
+        height: '400px',
+        position: 'right-top',
+        clickToClose: true,
+        distance: '20px',
+        fontSize: '16px',
+      });
+      addDisabledAtr(refs.startBtn);
+    } else {
+      removeDisabledAtr(refs.startBtn);
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+
+
+
+
+refs.startBtn.addEventListener('click', onClickStartTimer);
+
+function onClickStartTimer() {
+  setInterval(() => {
+    const currentData = new Date();
+    const timerData = selectedData - currentData;
+    if (timerData <= 0) {
+      refs.days.textContent = '00';
+      refs.hours.textContent = '00';
+      refs.minutes.textContent = '00';
+      refs.seconds.textContent = '00';
+      return;
+    }
+    const formatedData = convertMs(timerData);
+    console.log(formatedData);
+    runTimer(formatedData);
+  }, 1000);
+ 
+}
+
+function runTimer(data) {
+  refs.days.textContent = addLeadingZero(data.days);
+  refs.hours.textContent = addLeadingZero(data.hours);
+  refs.minutes.textContent = addLeadingZero(data.minutes);
+  refs.seconds.textContent = addLeadingZero(data.seconds);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, 0);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addDisabledAtr(linkOnBtn) {
+ 
+
+  linkOnBtn.disabled = true;
+}
+
+function removeDisabledAtr(linkOnBtn) {
+ 
+
+  linkOnBtn.disabled = false;
+}
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      });
+    delay += step;
   }
+}
+
+
+
+function createPromise(position, delay) {
+  const objectPromise = {position, delay}
+  const shouldResolve = Math.random() > 0.3;
+  const promis = new Promise((resolve, reject) => {
+
+    if (shouldResolve) {
+      resolve(objectPromise);
+    } else {
+      reject(objectPromise);
+    }
+  }, delay);
+  
+  return promis;
 }
