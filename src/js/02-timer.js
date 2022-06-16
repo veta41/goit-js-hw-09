f00ebc27161a3b9e17de6a4a976e53d46202a48d
veta41 <74262484+veta41@users.noreply.github.com>
2022-06-16 20:43:04 +0300

    fix task2  add themes

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/dark.css';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
